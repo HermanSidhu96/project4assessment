4754208bd894d3891d342ea62183c73b13e8e7fa
Your Name <you@example.com>
2020-10-02 17:37:17 -0400

    first commit

---
+++ src/circelselector.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default function CircleSelector(props)   {
+    return (
+        <div class="CircleSelector">
+            <button class="btn1">Select Circle 1</button>
+            <button class="btn2">Select Circle 2</button>
+            <button class="btn3">Select Circle 3</button>
+            <button class="btn4">Select Circle 4</button>
+        </div>
+    )
+}
---
+++ src/circles.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default function Circles(props)   {
+    return (
+        <div class= "Circles">
+            <div class= "circle1">1</div>
+            <div class= "circle2">2</div>
+            <div class= "circle3">3</div>
+            <div class= "circle4">4</div>
+        </div>
+    )
+}
